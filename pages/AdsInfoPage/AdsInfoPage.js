fc3252df65f91125e4f7232ad5c9ca727d8fb0df
Titeverstov <blnaxblachbl@gmail.com>
2018-01-24 00:06:45 +0900

    lala

--- pages/AdsInfoPage/AdsInfoPage.js
+++ pages/AdsInfoPage/AdsInfoPage.js
@@ -2,6 +2,11 @@ goBack = () => {
     favoriteRouter.goBack();
 }
 
+goAccount = () => {
+    favoriteRouter.push("otherAccount");
+}
+
 module.exports = {
     goBack: goBack,
+    goAccount: goAccount
 }
