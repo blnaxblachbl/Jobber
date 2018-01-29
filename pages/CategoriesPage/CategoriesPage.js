904cbd0a77428c1f4481b8574836f17bef1d9bbe
bairock <bairock@live.ru>
2018-01-29 21:23:07 +0900

    214124
    124124

--- pages/CategoriesPage/CategoriesPage.js
+++ pages/CategoriesPage/CategoriesPage.js
@@ -1,21 +1,15 @@
 let Observable = require("FuseJS/Observable");
 let Storage = require("FuseJS/Storage");
 let categories = Observable();
-let tokenValue = Observable();
-
-Storage.read("token").then(function (token) {
-    tokenValue.value = token
-}, function (error) {
-    console.log('token undefined')
-});
 
 getCategories = () => {
+    var token = Storage.readSync("myfile.txt");
     var status = 0;
     var response_ok = false;
     fetch('http://jobber.creatif.team/api/v1/categories', {
         method: 'POST',
         headers: { "Content-type": "application/json" },
-        body: JSON.stringify({ access_token: tokenValue.value })
+        body: JSON.stringify({ access_token: token })
     }).then(function (response) {
         status = response.status;  // Get the HTTP status code
         response_ok = response.ok; // Is response.status in the 200-range?
