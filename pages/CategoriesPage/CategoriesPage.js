e7788678bcc4ae374537e235493d48e004ae86c6
bairock <bairock@live.ru>
2018-01-30 23:36:09 +0900

    41241
    2412412

--- pages/CategoriesPage/CategoriesPage.js
+++ pages/CategoriesPage/CategoriesPage.js
@@ -2,28 +2,28 @@ let Observable = require("FuseJS/Observable");
 let Storage = require("FuseJS/Storage");
 let categories = Observable();
 
-Storage.read("token").then(function (data) {
-    var status = 0;
-    var response_ok = false;
-    fetch('http://192.168.1.11/api/v1/categories', {
-        method: 'POST',
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify({ access_token: data })
-    }).then(function (response) {
-        status = response.status;  // Get the HTTP status code
-        response_ok = response.ok; // Is response.status in the 200-range?
-        return response.json();    // This returns a promise
-    }).then(function (responseObject) {
-        if (responseObject.code == "200") {
-            categories.replaceAll(responseObject.content)
-            console.log(categories.value)
-        }
-    }).catch(function (err) {
-        // An error occurred somewhere in the Promise chain
-    });
-}, function (error) {
-    console.log('token undefined')
-});
+getCategories = () => {
+    var token = Storage.readSync("token");
+    if (token) {
+        var status = 0;
+        var response_ok = false;
+        fetch('http://192.168.1.11/api/v1/categories', {
+            method: 'POST',
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify({ access_token: token })
+        }).then(function (response) {
+            status = response.status;  // Get the HTTP status code
+            response_ok = response.ok; // Is response.status in the 200-range?
+            return response.json();    // This returns a promise
+        }).then(function (responseObject) {
+            if (responseObject.code == "200") {
+                categories.replaceAll(responseObject.content)
+            }
+        }).catch(function (err) {
+            // An error occurred somewhere in the Promise chain
+        });
+    }
+}
 
 goSubCategories = (val) => {
     sideRouter.push("sub", { subcategories: val.data.children, title: val.data.title })
@@ -32,4 +32,5 @@ goSubCategories = (val) => {
 module.exports = {
     categories: categories,
     goSubCategories: goSubCategories,
+    getCategories: getCategories
 }
